5ddb91d9bfc0db8033995bde6512c4c933ad17f7
Billy <xm9G@outlook.com>
2025-06-23 15:19:04 +1000

    made it work kinda

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+radio.set_group(1)
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'space'
+]
+message = ''
+count = 0
+def on_button_pressed_a2():
+    global count
+    global message
 
+    if input.button_is_pressed(Button.B):
+            global message
+            radio.send_string(message)
+            return
+
+    letter = letters[count]
+    basic.show_string(letter)
+    count +=1
+    
+input.on_button_pressed(Button.A, on_button_pressed_a2)
+
+def on_button_pressed_b2():
+    global message, count
+    letter = letters[count - 1]
+    if letter == 'space':
+        message += ' '
+    else:
+        message += letter
+    basic.show_string(message)
+    print(message)
+input.on_button_pressed(Button.B, on_button_pressed_b2)
+
+
+def on_received_string(receivedString):
+    basic.show_string(receivedString)
+radio.on_received_string(on_received_string)
--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+radio.setGroup(1)
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "space"]
+let message = ""
+let count = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a2() {
+    
+    
+    if (input.buttonIsPressed(Button.B)) {
+        
+        radio.sendString(message)
+        return
+    }
+    
+    let letter = letters[count]
+    basic.showString(letter)
+    count += 1
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b2() {
+    
+    let letter = letters[count - 1]
+    if (letter == "space") {
+        message += " "
+    } else {
+        message += letter
+    }
+    
+    basic.showString(message)
+    console.log(message)
+})
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    basic.showString(receivedString)
+})
